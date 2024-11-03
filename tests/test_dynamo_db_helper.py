33dd92897bfe709580f08c2e55d993b570046473
Bruno Lima <brunojet@gmail.com>
2024-11-02 21:23:57 -0300

    test

--- tests/test_dynamo_db_helper.py
+++ tests/test_dynamo_db_helper.py
@@ -1,20 +1,41 @@
+import boto3
 import pytest
-from unittest.mock import MagicMock, patch
-from utils.dynamo_db_utils import dynamodb_with_range_key, DESCRIBLE_TABLE_MOCK
+from typing import Any, Tuple
+from moto import mock_aws
+from unittest.mock import patch
+from dynamo_db_utils import create_table, DESCRIBLE_TABLE
 from dynamo_db_helper import DynamoDBHelper, PRIMARY_HASH_KEY, PRIMARY_RANGE_KEY
 
 
-class TestDynamoDBHelper(DynamoDBHelper):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+@pytest.fixture
+def dynamodb():
+    with mock_aws():
+        resource = boto3.resource("dynamodb", region_name="us-east-1")
+        client = boto3.client("dynamodb", region_name="us-east-1")
+
+        key_schema = {"HASH": "id", "RANGE": "id_range"}
+        gsi_key_schemas = [
+            {
+                "index_name": "gsi_hash_key-gsi_range_key-index",
+                "HASH": "gsi_hash_key",
+                "RANGE": "gsi_range_key",
+            },
+        ]
+        table = create_table(resource, key_schema, gsi_key_schemas)
+
+        yield client, table
 
 
 @pytest.fixture
-def dynamo_db_helper(dynamodb_with_range_key):
+def dynamo_db_helper(dynamodb):
+    client, table = dynamodb
+
     with patch.object(
-        dynamodb_with_range_key.meta.client, "describe_table", return_value=DESCRIBLE_TABLE_MOCK
+        client,
+        "describe_table",
+        return_value=DESCRIBLE_TABLE,
     ):
-        yield TestDynamoDBHelper(
+        repo = DynamoDBHelper(
             table_name="test_table",
             max_item_size=256,
             has_range_key=True,
@@ -24,59 +45,71 @@ def dynamo_db_helper(dynamodb_with_range_key):
             ],
         )
 
-
-def test_init(dynamo_db_helper):
-    assert dynamo_db_helper.primary_keys == [PRIMARY_HASH_KEY, PRIMARY_RANGE_KEY]
-    assert dynamo_db_helper.has_range_key is True
-    assert dynamo_db_helper.range_key_items == ["range_key1", "range_key2"]
-    assert len(dynamo_db_helper.gsi_key_schemas) == 1
+        yield repo, table
 
 
-def test_is_primary_key(dynamo_db_helper):
+def test_init(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
+    assert repo.primary_keys == [PRIMARY_HASH_KEY, PRIMARY_RANGE_KEY]
+    assert repo.has_range_key is True
+    assert repo.range_key_items == ["range_key1", "range_key2"]
+    assert len(repo.gsi_key_schemas) == 1
+
+
+def test_is_primary_key(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
+
     key_condition = {PRIMARY_HASH_KEY: "hash_value", PRIMARY_RANGE_KEY: "range_value"}
-    assert dynamo_db_helper.is_primary_key(key_condition) is True
+    assert repo.is_primary_key(key_condition) is True
 
     key_condition = {PRIMARY_HASH_KEY: "hash_value"}
-    assert dynamo_db_helper.is_primary_key(key_condition) is False
+    assert repo.is_primary_key(key_condition) is False
 
 
-def test_add_range_key(dynamo_db_helper):
+def test_add_range_key(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
     item = {
         PRIMARY_HASH_KEY: "hash_value",
         "range_key1": "value1",
         "range_key2": "value2",
     }
-    dynamo_db_helper.add_range_key(item)
+    repo.add_range_key(item)
     assert item[PRIMARY_RANGE_KEY] == "value1#value2"
 
 
-def test_build_insert_condition_expression(dynamo_db_helper):
-    condition_expression = dynamo_db_helper._build_insert_condition_expression()
+def test_build_insert_condition_expression(
+    dynamo_db_helper: Tuple[DynamoDBHelper, Any]
+):
+    repo = dynamo_db_helper[0]
+    condition_expression = repo._build_insert_condition_expression()
     assert condition_expression is not None
 
 
-def test_build_key_expression(dynamo_db_helper):
+def test_build_key_expression(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
     key_condition = {PRIMARY_HASH_KEY: "hash_value", PRIMARY_RANGE_KEY: "range_value"}
-    key_expression = dynamo_db_helper._build_key_expression(key_condition)
+    key_expression = repo._build_key_expression(key_condition)
     assert key_expression is not None
 
 
-def test_get_gsi_key_schema(dynamo_db_helper):
+def test_get_gsi_key_schema(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
     key_set = {"gsi_hash_key", "gsi_range_key"}
-    gsi_key_schema = dynamo_db_helper._get_gsi_key_schema(key_set)
+    gsi_key_schema = repo._get_gsi_key_schema(key_set)
     assert gsi_key_schema is not None
 
 
-def test_get_gsi_key_expression(dynamo_db_helper):
+def test_get_gsi_key_expression(dynamo_db_helper: Tuple[DynamoDBHelper, Any]):
+    repo = dynamo_db_helper[0]
     key_condition = {"gsi_hash_key": "hash_value", "gsi_range_key": "range_value"}
-    index_name, key_expression = dynamo_db_helper._get_gsi_key_expression(key_condition)
+    index_name, key_expression = repo._get_gsi_key_expression(key_condition)
     assert index_name == "GSI1"
     assert key_expression is not None
 
 
 def test_build_projection_expression():
     projection_expression = ["id", "name", "status"]
-    expression, attribute_names = TestDynamoDBHelper.build_projection_expression(
+    expression, attribute_names = DynamoDBHelper.build_projection_expression(
         projection_expression
     )
     assert expression == "id, name, #status"
